777782a290986a5606720159ce9fd92ad4548f5a
Jeremy <jsilliman@protonmail.com>
2019-09-03 11:42:24 -0400

    Implement Modutil

--- modules/modutil.py
+++ modules/modutil.py
@@ -1,5 +1,6 @@
+import discord.member
 from discord.ext import commands
-from modules.util import config, write_message
+from modules.util import config, check_roles, write_message, list_prettyprint
 import re
 
 
@@ -7,10 +8,84 @@ class ModUtil(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
-    #TODO add proper error handling to this def
+    @commands.has_permissions(kick_members=True)
+    @commands.command()
+    async def kick_neo(self, ctx, users: commands.Greedy[discord.Member], *, reason: str = None):
+        """
+        Kicks users from the server.
+        """
+        for user in users:
+            await ctx.guild.kick(user, reason=f'{reason + " " if reason else ""}(Issued by {ctx.author.name})')
+        await ctx.send(f'{list_prettyprint(user.name for user in users)} kicked.')
+
+    @commands.has_permissions(ban_members=True)
+    @commands.command()
+    async def ban_neo(self, ctx, users: commands.Greedy[discord.User], *, reason: str = None):
+        """
+        Bans users from the server.
+        """
+        for user in users:
+            await ctx.guild.ban(user, reason=f'{reason + " " if reason else ""}(Issued by {ctx.author.name})',
+                                delete_message_days=0)
+        await ctx.send(f'{list_prettyprint(user.name for user in users)} banned.')
+
+    @commands.has_permissions(ban_members=True)
+    @commands.command()
+    async def unban_neo(self, ctx, users: commands.Greedy[discord.User], *, reason: str = None):
+        """
+        Unbans users from the server.
+        """
+        for user in users:
+            await ctx.guild.unban(user, reason=f'{reason + " " if reason else ""}(Issued by {ctx.author.name})')
+        await ctx.send(f'{list_prettyprint(user.name for user in users)} unbanned.')
+
+    @check_roles('pins')
+    @commands.command()
+    async def pin_neo(self, ctx, ids: commands.Greedy[discord.TextChannel]):
+        """
+        Pins a message in a channel.
+        """
+        server = config[str(ctx.guild.id)]
+        if ctx.channel.id not in server['pins']['channels'] and not \
+                (role in ctx.author.roles for role in server['staff']['roles']):
+            return
+        for id in ids:
+            await ctx.message.channel.fetch_message(id).pin()
+        await ctx.send(f"{list_prettyprint(ids)} pinned.")
+
+    @check_roles('pins')
+    @commands.command()
+    async def unpin_neo(self, ctx, ids: commands.Greedy[discord.TextChannel]):
+        """
+        Unpins a message in a channel.
+        """
+        server = config[str(ctx.guild.id)]
+        if ctx.channel.id not in server['pins']['channels'] and not \
+                (role in ctx.author.roles for role in server['staff']['roles']):
+            return
+        for id in ids:
+            await ctx.message.channel.fetch_message(id).unpin()
+        await ctx.send(f"{list_prettyprint(ids)} unpinned.")
+
+    @check_roles('staff')
+    @commands.command()
+    async def purge_neo(self, ctx, users: commands.Greedy[discord.Member], num: int):
+        """
+        Purges messages from the current channel.
+        """
+
+        def check(message):
+            return message.author in users if users else True
+        deletes = await ctx.channel.purge(limit=num + 1, check=check)
+        await ctx.send(f'Deleted {len(deletes - 1)} message{"" if len(deletes) == 1 else "s"}'
+                       f'{(" by " + list_prettyprint(user.name for user in users) if users else "")}.')
+
     @commands.is_owner()
     @commands.command()
     async def announce(self, ctx, *, announcement):
+        """
+        Announces a message to every server Jerbot is deployed in. Owner only.
+        """
         for server in config:
             if bool(re.search(r'\d', server)):
                 try:
@@ -19,7 +94,25 @@ class ModUtil(commands.Cog):
                     await ctx.send('**CONFIGURATION ERROR**: '
                                    f'{self.bot.get_guild(server).name} does not have a valid mod channel.' )
 
-    
+    @commands.is_owner()
+    @commands.command()
+    async def global_ban(self, ctx, users: commands.Greedy[discord.User], *, reason: str = None):
+        """
+        Bans a user from every server Jerbot is deployed in. Dangerous. Owner only.
+        Only use in the event that a user is a known raider or part of a spambot.
+        """
+        if not reason:
+            reason = "No reason provided."
+
+        edit_message = await ctx.send("Global in progress. This may take a long time.")
+        for guild in self.bot.guilds:
+            try:
+                for user in users:
+                    await guild.ban(user, reason=f"Global ban: {reason}")
+            except discord.Forbidden:
+                pass
+        await edit_message.edit(content=f'{list_prettyprint(user.name for user in users)} globally banned.')
+
 
 def setup(bot):
     bot.add_cog(ModUtil(bot))
