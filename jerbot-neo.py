41aa12df6d2120711efd9556e9df42996359e49f
Jeremy <jsilliman@protonmail.com>
2019-09-06 10:22:35 -0400

    jerbot-neo: experimental error handler

--- jerbot-neo.py
+++ jerbot-neo.py
@@ -1,9 +1,8 @@
 import logging.handlers
-import os
-import re
-import yaml
+from sqlite3 import OperationalError
 
 from discord.ext import commands
+from discord import Forbidden
 from modules.util import load_config, load_modules, bot, config
 
 logger = logging.getLogger('discord')
@@ -17,18 +16,39 @@ logger.addHandler(handler)
 async def on_ready():
     print(f'Ready.\nLogged in as {bot.user.name}')
 
+
+@bot.event
+async def on_command_error(ctx, err):
+
+    cmd = ctx.command
+    err = getattr(err, 'original', err)
+
+    if isinstance(err, commands.CommandNotFound):
+        return
+    elif isinstance(err, OperationalError):
+        await ctx.send(f'{cmd} failed due to a database error: {err}')
+    elif isinstance(err, commands.MissingPermissions) or isinstance(err, commands.CheckFailure):
+        await ctx.send(f"You have insufficient permissions to run {cmd}.")
+    elif isinstance(err, commands.BadArgument):
+        await ctx.send(f"Bad argument was given to {cmd}.")
+        await ctx.send_help(cmd)
+    elif isinstance(err, commands.MissingRequiredArgument):
+        await ctx.send(f"An argument required for {cmd} was missing.")
+        await ctx.send_help(cmd)
+    elif isinstance(err, Forbidden):
+        await ctx.send(f"Bot is missing permissions necessary to run this command. {err}.")
+    else:
+        await ctx.send(f'An unexpected error occured when running {cmd}: {err}')
+
 @bot.event
 async def on_message(message):
     # jerbot-neo can dynamically change bot prefixes per-server, we are hijacking on_message to change it
     bot.command_prefix = config[str(message.guild.id)]['prefix']
     await bot.process_commands(message)
 
-# TODO *global* error handling, none of this try/except trash every two lines
-# TODO add docstrings
-# TODO proper permission management this time
-
 print('Initializing...')
 print(f'Successfully loaded {" ".join(load_config())}' if load_config() else 'No config loaded.')
 print(f'Successfully loaded {" ".join(load_modules())}')
+
 
 bot.run(config['main']['token'])
