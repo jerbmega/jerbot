37b0be609e28b94b1a7c9baa0f796cbe96d6b668
Jeremy <jsilliman@protonmail.com>
2020-03-15 16:39:05 -0400

    [platgod] Rewrite platgod Platgod now uses an embed that can change between search results using reactions at the bottom of the message. This bypasses worry about the 2000 character limit with multiple results, allows for a theoretically infinite amount of results, and generally makes everything look cleaner.

--- modules/platgod.py
+++ modules/platgod.py
@@ -2,7 +2,7 @@ from discord.ext import commands
 import requests
 from bs4 import BeautifulSoup
 import modules.db as db
-import discord.member
+from modules.util import config, write_embed
 
 
 class PlatGod(commands.Cog):
@@ -27,12 +27,39 @@ class PlatGod(commands.Cog):
         page = "original" if parse[0] in original else "rebirth" if parse[0] in rebirth \
             else "afterbirth" if parse[0] in afterbirth else "antibirth" if parse[0] in anti else "afterbirthplus"
         parse = ' '.join(parse) if page == "afterbirthplus" else parse[1]
-        response = db.query(f'select detail from platgod_{page} where item like "%{parse}%"')
-        response[0] = f'**{response[0]}**'
-        try:
-            await ctx.send('\n'.join(response))
-        except discord.HTTPException:
-            await ctx.send("This query is too broad. Please try again.")
+        items = db.query(f'select distinct item from platgod_{page} where item like "%{parse}%"')
+        if not items:
+            await ctx.send("No results found.")
+            return
+        fields = [[items[0], "\n".join(db.query(f'select detail from platgod_{page} where item ="{items[0]}"')[1:])]]
+
+        embed = await write_embed(ctx.channel.id, None, config['main']['embed_color'], f"1/{len(items)}",
+                                  f"Search results for {parse} [{page}]", fields=fields, avatar=False)
+        if len(items) > 1:
+            await embed.add_reaction("⬅️")
+            await embed.add_reaction("➡️")
+
+    @commands.Cog.listener()
+    async def on_reaction_add(self, reaction, user):
+        if user == self.bot.user:
+            return
+        await reaction.message.remove_reaction(reaction.emoji, user)
+
+        for embed in reaction.message.embeds:
+            if "Search results for" in embed.author.name:
+                page = embed.title.split("/")
+                page = [int(page[0]), int(page[1])]
+                page[0] = page[0] - 1 if reaction.emoji == '⬅️' else page[0] + 1 if reaction.emoji == "➡️" else page[0]
+                page[0] = 1 if page[0] < 1 else page[1] if page[0] > page[1] else page[0]
+                embed.title = f'{page[0]}/{page[1]}'
+                embed.clear_fields()
+                item = embed.author.name.split("Search results for ")[1].split(" [")[0]
+                ver = embed.author.name.split("[")[1][:-1]
+                items = db.query(f'select distinct item from platgod_{ver} '
+                                 f'where item like "%{item}%"')
+                embed.add_field(name=items[page[0] - 1], value="\n".join(
+                    db.query(f'select detail from platgod_{ver} where item ="{items[page[0] - 1]}"')[1:]))
+                await reaction.message.edit(embed=embed)
 
     @commands.command()
     @commands.is_owner()
