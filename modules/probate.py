7125cbfee9ca024b28c9163c9beb1dcfe8eddb26
Jeremy <jsilliman@protonmail.com>
2020-12-27 14:32:44 -0500

    [probate] Add aliases (s/probate/timeout)

--- modules/probate.py
+++ modules/probate.py
@@ -12,7 +12,7 @@ class Probation(commands.Cog):
         return check_roles('probate', ctx) and await module_enabled('probate', ctx.guild.id)
 
 
-    @commands.command()
+    @commands.command(aliases=['timeout'])
     async def probate(self, ctx, users: commands.Greedy[discord.Member], time: str = "24h", *, reason=""):
         """
         Locks users behind a probation role. This role will automatically be re-added if the user attempts to leave.
@@ -22,7 +22,7 @@ class Probation(commands.Cog):
             await probate_user(user, time, reason)
         await ctx.send(f'{list_prettyprint(user.name for user in users)} banished to the Shadow Realm.')
 
-    @commands.command()
+    @commands.command(aliases=['untimeout'])
     async def unprobate(self, ctx, users: commands.Greedy[discord.Member]):
         """
         Immediately removes a user from probation, canceling the scheduled job.
@@ -31,7 +31,7 @@ class Probation(commands.Cog):
             await unprobate_user(user)
         await ctx.send(f'{list_prettyprint(user.name for user in users)} released from the Shadow Realm.')
 
-    @commands.command()
+    @commands.command(aliases=['autotimeout'])
     async def autoprobate(self, ctx, state: bool = None):
         """
         Enables auto-probation. This will automatically add users to the database when they join.
@@ -44,20 +44,14 @@ class Probation(commands.Cog):
     @commands.Cog.listener()
     async def on_member_join(self, member):
         server = config[str(member.guild.id)]
-        if not module_enabled('probate', member.guild.id): return
-        query = db.query(f'SELECT * from probations_{member.guild.id}')
+        if not await module_enabled('probate', member.guild.id): return
         if member.id in db.query(f'SELECT DISTINCT id FROM probations_{member.guild.id}'):
             await self.bot.get_channel(server['modlog_id']).send('Permission bypass detected, '
                                                                  f'probating {member.mention} and resetting timer.')
-
-            time = await parse_time(db.query(f'SELECT DISTINCT time FROM probations_{member.guild.id} '
-                                         f'WHERE id = {member.id}')[0])
-            reason = db.query(f'SELECT DISTINCT reason FROM probations_{member.guild.id} where id = {member.id}'[0])
+            time = db.query(f'SELECT DISTINCT time FROM probations_{member.guild.id} '
+                                         f'WHERE id = {member.id}')[0]
+            reason = db.query(f'SELECT DISTINCT reason FROM probations_{member.guild.id} where id = {member.id}')[0]
             await probate_user(member, time, reason)
-            await schedule_task(unprobate_user, time,
-                                f'probation_{member.guild.id}_{member.id}', [member])
-            await member.add_roles(discord.utils.get(member.guild.roles,
-                                                   name=server['probate']['role_name']))
             await member.send(f'You left {member.guild.name} while you were in probation, so the role has been '
                               'automatically re-applied. Additionally, your probation timer has reset.')
 
@@ -68,11 +62,14 @@ class Probation(commands.Cog):
             await self.bot.get_channel(server['modlog_id']).send(f'Auto-probation enabled, probating {member.mention}.')
 
     @commands.Cog.listener()
-    async def on_member_leave(self, member):
-        if module_enabled('probate', member.guild.id) and member.id in db.query(
-                f'SELECT DISTINCT id FROM probations_{member.guild.id}'):
+    async def on_member_remove(self, member):
+        if not await module_enabled('probate', member.guild.id): return
+        try:
             await remove_task(f'probation_{member.guild.id}_{member.id}')
-            channel = discord.utils.get(member.guild.text_channels, name=f"probation_{member.id}")
+        except Exception as e:
+            print(Exception)
+        channel = discord.utils.get(member.guild.channels, name=f"probation_{member.id}")
+        if channel:
             await channel.delete()
 
 def setup(bot):
