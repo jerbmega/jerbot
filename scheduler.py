295fa8d2d416aa8b345115d88ec442fbb7692709
Jeremy Silliman <jsilliman@protonmail.com>
2022-11-28 08:26:35 -0500

    scheduler: use async jobstore

--- scheduler.py
+++ scheduler.py
@@ -3,7 +3,9 @@ from apscheduler.jobstores.sqlalchemy import SQLAlchemyJobStore
 
 scheduler = AsyncIOScheduler()
 
-scheduler.add_jobstore(SQLAlchemyJobStore(url="sqlite:///db/scheduler.db"), "default")
+scheduler.add_jobstore(
+    SQLAlchemyJobStore(url="sqlite+aiosqlite:///db/scheduler.db"), "default"
+)
 scheduler.configure(
     # APScheduler doesn't support just running a job if the bot had downtime, here's a nasty workaround! :kawaii: :compressed_torvalds: etc
     # This should be equivalent to the amount of seconds in a non-leap year... the bot ain't gonna be down for a year so this should be sufficient
